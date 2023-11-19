ca4d77cef30daae362b232f1a5ebd04f62ee44d4
lasrca <carla.lasry@ispot.tv>
2023-11-19 10:56:04 +0100

    Update test_transformers_vit.py

--- scripts/test_transformers_vit.py
+++ scripts/test_transformers_vit.py
@@ -72,12 +72,12 @@ def get_filenames(source_dir):
 def get_all_results(caps_filenames, caps_images, outputs_caps, streams_filenames, streams_images, outputs_streams):
     results = {}
     for cap_filename, cap_img, cap in zip(caps_filenames, caps_images, outputs_caps):
-        cap_sess_id = cap_filename.split("_")[0]
-        results[cap_filename] = {}
+        cap_sess_id = cap_filename.split("_")[0]+"_cap"
+        results[cap_sess_id] = {}
         for stream_filename, stream_img, stream in zip(streams_filenames, streams_images, outputs_streams):
             cosine_similarity = get_cosine_similarity_for_two_images(cap.tolist(), stream.tolist())
-            stream_sess_id = stream_filename.split("_")[0]
-            results[cap_filename][stream_filename] = cosine_similarity
+            stream_sess_id = stream_filename.split("_")[0] + "_stream"
+            results[cap_sess_id][stream_sess_id] = cosine_similarity
     return results
 
 
@@ -100,10 +100,10 @@ def main():
     model = model.to(device)
 
     print("Loading images...")
-    caps_imgs = load_images_folder(caps_path)[:5]
-    caps_filenames = get_filenames(caps_path)[:5]
-    streams_imgs = load_images_folder(streams_path)[:5]
-    streams_filenames = get_filenames(streams_path)[:5]
+    caps_imgs = load_images_folder(caps_path)
+    caps_filenames = get_filenames(caps_path)
+    streams_imgs = load_images_folder(streams_path)
+    streams_filenames = get_filenames(streams_path)
 
     print("Processing input...")
     inputs_cap = process_input(caps_imgs, processor)
